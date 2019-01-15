06eda7ed491db802aa0de285d2082a328dca7a34
janwrx <janzosa@gmail.com>
2019-01-15 10:37:09 -0800

    updated required form and state examples

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -7,13 +7,13 @@ import TextField from '@material-ui/core/TextField';
 
 const styles = theme => ({
   container: {
-    display: 'flex',
+    display: 'grid',
     flexWrap: 'wrap',
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 500,
+    width: 200,
   },
   dense: {
     marginTop: 19,
@@ -21,33 +21,37 @@ const styles = theme => ({
   menu: {
     width: 200,
   },
+  
 });
 
-const currencies = [
+const states = [
   {
-    value: 'USD',
-    label: '$',
+    value: '',
+    label: '',
+      },
+  {
+    value: 'Alabama',
+    label: 'AL',
   },
   {
-    value: 'EUR',
-    label: '€',
+    value: 'California',
+    label: 'CA',
   },
   {
-    value: 'BTC',
-    label: '฿',
+    value: 'New York',
+    label: 'NY',
   },
   {
-    value: 'JPY',
-    label: '¥',
+    value: 'Hawaii',
+    label: 'HI',
   },
 ];
 
 class TextFields extends React.Component {
   state = {
     name: 'Cat in the Hat',
-    age: '',
+    state: '',
     multiline: 'Controlled',
-    currency: 'EUR',
   };
 
   handleChange = name => event => {
@@ -60,6 +64,7 @@ class TextFields extends React.Component {
     const { classes } = this.props;
 
     return (
+        <div>
       <form className={classes.container} noValidate autoComplete="off">
        <TextField
           required
@@ -94,160 +99,60 @@ class TextFields extends React.Component {
           margin="normal"
         />
         <TextField
+          id="standard"
+          label="Unit/Apt. #"
+          className={classes.textField}
+          margin="normal"
+        />
+       <TextField
           required
           id="standard-required"
-          label="First name"
-          placeholder="ex. John"
+          label="City"
           className={classes.textField}
           margin="normal"
         />
         <TextField
-          id="standard-password-input"
-          label="Password1"
+          id="standard-select-state"
+          select required
+          label="State"
           className={classes.textField}
-          type="password1"
-          autoComplete="current-password"
-          margin="normal"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          className={classes.textField}
-          margin="normal"
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        <TextField
-          id="standard-dense"
-          label="Dense"
-          className={classNames(classes.textField, classes.dense)}
-          margin="dense"
-        />
-        <TextField
-          id="standard-multiline-flexible"
-          label="Multiline"
-          multiline
-          rowsMax="4"
-          value={this.state.multiline}
-          onChange={this.handleChange('multiline')}
-          className={classes.textField}
-          margin="normal"
-        />
-        <TextField
-          id="standard-multiline-static"
-          label="Multiline"
-          multiline
-          rows="4"
-          defaultValue="Default Value"
-          className={classes.textField}
-          margin="normal"
-        />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Some important text"
-          margin="normal"
-        />
-        <TextField
-          id="standard-with-placeholder"
-          label="With placeholder"
-          placeholder="Placeholder"
-          className={classes.textField}
-          margin="normal"
-        />
-        <TextField
-          id="standard-textarea"
-          label="With placeholder multiline"
-          placeholder="Placeholder"
-          multiline
-          className={classes.textField}
-          margin="normal"
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          value={this.state.age}
-          onChange={this.handleChange('age')}
-          type="number"
-          className={classes.textField}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          margin="normal"
-        />
-        <TextField
-          id="standard-search"
-          label="Search field"
-          type="search"
-          className={classes.textField}
-          margin="normal"
-        />
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Select"
-          className={classes.textField}
-          value={this.state.currency}
-          onChange={this.handleChange('currency')}
+          value={this.state.state}
+          onChange={this.handleChange('state')}
           SelectProps={{
             MenuProps: {
               className: classes.menu,
             },
           }}
-          helperText="Please select your currency"
+          helperText="Please select your state"
           margin="normal"
         >
-          {currencies.map(option => (
+          {states.map(option => (
             <MenuItem key={option.value} value={option.value}>
               {option.label}
             </MenuItem>
           ))}
         </TextField>
-        <TextField
-          id="standard-select-currency-native"
-          select
-          label="Native select"
+       <TextField
+          required
+          id="standard-required"
+          label="Postal Code"
+          placeholder="55555"
           className={classes.textField}
-          value={this.state.currency}
-          onChange={this.handleChange('currency')}
-          SelectProps={{
-            native: true,
-            MenuProps: {
-              className: classes.menu,
-            },
-          }}
-          helperText="Please select your currency"
           margin="normal"
-        >
-          {currencies.map(option => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </TextField>
-        <TextField
-          id="standard-full-width"
-          label="Label"
-          style={{ margin: 8 }}
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
         />
         <TextField
-          id="standard-bare"
+          required
+          id="standard-email-input"
+          label="Email"
           className={classes.textField}
-          defaultValue="Bare"
+          type="email"
+          name="email"
+          autoComplete="email"
           margin="normal"
         />
       </form>
+    
+      </div>
     );
   }
 }
